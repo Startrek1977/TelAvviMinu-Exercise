953d7ac3f1b3c6677c41549cadbeeda9ce7bebed
Startrek1977 <iro1977@gmail.com>
2026-01-09 08:42:59 +0200

    Initial solution with one empty project.

---
+++ TelAvivMuni-Exercise/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TelAvivMuni_Exercise;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
